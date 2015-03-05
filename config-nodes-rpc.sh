547f51297a0e9beb72b47d202ac3cbeddbe4b379
tecsploit <tecsploit@tecsploit.xyz>
2015-03-05 21:22:19 +0800

    config nodes and rpc script

---
+++ config-nodes-rpc.sh
@@ -0,0 +1,22 @@
+echo "checkpoints=o" >> ~/.tecoin/tecoin.conf
+echo "testnet=1" >> ~/.tecoin/tecoin.conf
+echo "daemon=1" >> ~/.tecoin/tecoin.conf
+echo "rpcport=10098" >> ~/.tecoin/tecoin.conf
+echo "rpcuser=test" >> ~/.tecoin/tecoin.conf
+echo "rpcpassword=123456" >> ~/.tecoin/tecoin.conf
+echo "rpcallowip=127.0.0.1" >> ~/.tecoin/tecoin.conf
+echo "addnode=node1.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node2.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node3.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node4.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node5.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node6.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node7.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node8.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node9.tecoin.net" >> ~/.tecoin/tecoin.conf
+echo "addnode=node10.tecoin.net" >> ~/.tecoin/tecoin.conf
+
+
+
+
+
